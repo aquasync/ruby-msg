c8f8491182c51423f16db6d0a91ce04bdfc5b3e3
aquasync <aquasync@gmail.com>
2012-07-03 23:22:08 +1000

    Bump version and update ChangeLog.

--- lib/mapi.rb
+++ lib/mapi.rb
@@ -2,7 +2,7 @@ require 'mapi/types'
 require 'mapi/property_set'
 
 module Mapi
-	VERSION = '1.5.0'
+	VERSION = '1.5.1'
 
 	#
 	# Mapi::Item is the base class used for all mapi objects, and is purely a
